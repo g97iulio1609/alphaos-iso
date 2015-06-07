9a3fb286f04748bbab69f74760d62fc342285c10
root <root@localhost.localdomain>
2015-06-07 21:34:59 +0200

    sudo,more

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -13,7 +13,6 @@ useradd -m -g users -G "audio,disk,optical,wheel,network,autologin" alpha
 usermod -s /usr/bin/zsh alpha
 cp -aT /etc/skel/ /root/
 chmod 700 /root
-echo "alpha ALL=(ALL) ALL" >> /etc/sudoers
 chown root:root /etc/sudoers 
 chmod 440 /etc/sudoers
 chown -R root:root /etc/sudoers.d
@@ -21,8 +20,8 @@ chmod  755 /etc/sudoers.d
 chmod  440 /etc/sudoers.d/*
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
-systemctl enable  lightdm.service NetworkManager.service pacman-init.service choose-mirror.service
+systemctl enable  lightdm.service org.cups.cupsd.service NetworkManager.service pacman-init.service choose-mirror.service
 systemctl --user enable pulseaudio.socket
 systemctl set-default multi-user.target
 systemctl set-default graphical.target
-gsettings set org.gnome.shell.extensions.user-theme name "alphaos-ux"
+
